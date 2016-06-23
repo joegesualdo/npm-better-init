3c64475b19699533ad3aab392099c1c01ef2e39a
Joe Gesualdo <joegesualdo@gmail.com>
2016-06-23 19:54:05 -0400

    Remove useless dir

--- woowee/broo/test-test2/test.js
+++
@@ -1,6 +0,0 @@
-import test from 'ava';
-import test-test2 from './dist'
-
-test(t => {
-    t.deepEqual([1, 2], [1, 2]);
-});
--- woowee/broo/test.js
+++
@@ -1,6 +0,0 @@
-import test from 'ava';
-import broo from './dist'
-
-test(t => {
-    t.deepEqual([1, 2], [1, 2]);
-});
